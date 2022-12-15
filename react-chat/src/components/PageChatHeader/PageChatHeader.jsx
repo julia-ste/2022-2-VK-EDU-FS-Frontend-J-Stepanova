c87591f128510f0e29d05f8b30b05b38b5fea795
Julia Stepanova <yuliya.stepanova@phystech.edu>
2022-12-15 18:24:49 +0300

    Add routing

--- react-chat/src/components/PageChatHeader/PageChatHeader.jsx
+++ react-chat/src/components/PageChatHeader/PageChatHeader.jsx
@@ -1,35 +1,40 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { ArrowBack, MoreVert, Search } from '@mui/icons-material'
 
 import styles from './PageChatHeader.module.scss'
 
 
-const PageChatHeader = ({ title, subtitle, imgSrc, onArrowBackClick }) => (
-    <>
-        <button className={styles.button} onClick={onArrowBackClick}>
-            <ArrowBack className={styles.icon} />
-        </button>
-        <div className={styles.container}>
-            <a href="/#">
-                <img alt="" className={styles.img} src={imgSrc} />
-            </a>
-            <div className={styles.info}>
-                <a className={styles.title} href="/#">
-                    {title}
+const PageChatHeader = ({ title, subtitle, imgSrc }) => {
+    const navigate = useNavigate()
+
+    return (
+        <>
+            <button className={styles.button} onClick={() => navigate('/')}>
+                <ArrowBack className={styles.icon} />
+            </button>
+            <div className={styles.container}>
+                <a href="/#">
+                    <img alt="" className={styles.img} src={imgSrc} />
                 </a>
-                <div className={styles.subtitle}>{subtitle}</div>
+                <div className={styles.info}>
+                    <a className={styles.title} href="/#">
+                        {title}
+                    </a>
+                    <div className={styles.subtitle}>{subtitle}</div>
+                </div>
             </div>
-        </div>
-        <div className={styles.actions}>
-            <button className={styles.button}>
-                <Search className={styles.icon} />
-            </button>
-            <button className={styles.button}>
-                <MoreVert className={styles.icon} />
-            </button>
-        </div>
-    </>
-)
+            <div className={styles.actions}>
+                <button className={styles.button}>
+                    <Search className={styles.icon} />
+                </button>
+                <button className={styles.button}>
+                    <MoreVert className={styles.icon} />
+                </button>
+            </div>
+        </>
+    )
+}
 
 export default PageChatHeader
